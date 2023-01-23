c0c7fbd22e7ceff64e49e0e7a81ffbc2f6120512
Jakub <Jakub@JK-HPLAPTOP>
2023-01-23 22:57:25 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_Helpdesk_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
