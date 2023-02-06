7b1b031b14fa15266d6530621fadb5de65caf7aa
Jakub Kołodziej <jakub.kolodziej28@microsoft.wsei.edu.pl>
2023-02-06 19:14:19 +0100

    Before_FirstMigration

---
+++ Entities/Ticket.cs
@@ -0,0 +1,18 @@
+﻿namespace Project_Helpdesk_Portal.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public int DeviceId { get; set; }
+
+        public int StatusId { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User Users { get; set; }
+    }
+}
