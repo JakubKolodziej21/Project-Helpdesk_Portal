9fefdae4523f001243603f0b84965cdcd8aef74f
Jakub Kołodziej <jakub.kolodziej28@microsoft.wsei.edu.pl>
2023-02-16 17:59:59 +0100

    WorkingLoggingPage

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -28,7 +28,7 @@ namespace Project_Helpdesk_Portal.Controllers
         {
 
             string connString = "Server=(localdb)\\mssqllocaldb;Database=HelpdeskDb;Trusted_Connection=True;TrustServerCertificate=True;";
-            string query = "SELECT COUNT(*) FROM Users WHERE Email = @Email AND Password = @Password";
+            string query = "SELECT COUNT(*) From (SELECT * FROM Users WHERE Email = @Email AND Password = @Password Union All SELECT * FROM Users WHERE Email = @Email AND Password = @Password And IsAdmin = 1)AS combined_results ";
 
             using (SqlConnection conn = new SqlConnection(connString))
             using (SqlCommand cmd = new SqlCommand(query, conn))
@@ -62,8 +62,40 @@ namespace Project_Helpdesk_Portal.Controllers
 
                     
 
+                    return RedirectToAction("Create", "Tickets");
+                }
+                if (count == 2)
+                {
+                    List<Claim> claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                    new Claim("OtherProperties","Example Role")
+
+                };
+
+                    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,
+                        CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    AuthenticationProperties properties = new AuthenticationProperties()
+                    {
+
+                        AllowRefresh = true,
+                        IsPersistent = modelLogin.IsAdmin && modelLogin.IsHelpdeskStaff
+                    };
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity), properties);
+
+
+
                     return RedirectToAction("Index", "Tickets");
                 }
+
+
+
+
+
+
+
 
 
 
