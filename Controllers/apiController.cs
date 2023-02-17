bed50f5f4dc87853fd2d46d373c3fbfb86b18b83
Jakub Kołodziej <jakub.kolodziej28@microsoft.wsei.edu.pl>
2023-02-17 19:43:17 +0100

    WorkingApi

---
+++ Controllers/apiController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Helpdesk_Portal.Data;
+using Project_Helpdesk_Portal.Models;
+
+namespace Project_Helpdesk_Portal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class apiController : ControllerBase
+    {
+        private readonly HelpdeskDbContext _context;
+
+        public apiController(HelpdeskDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/api
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+        {
+            return await _context.Tickets.ToListAsync();
+        }
+
+        // GET: api/api/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ticket>> GetTicket(int id)
+        {
+            var ticket = await _context.Tickets.FindAsync(id);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return ticket;
+        }
+
+        // PUT: api/api/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTicket(int id, Ticket ticket)
+        {
+            if (id != ticket.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ticket).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/api
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Ticket>> PostTicket(Ticket ticket)
+        {
+            _context.Tickets.Add(ticket);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTicket", new { id = ticket.Id }, ticket);
+        }
+
+        // DELETE: api/api/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTicket(int id)
+        {
+            var ticket = await _context.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tickets.Remove(ticket);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TicketExists(int id)
+        {
+            return _context.Tickets.Any(e => e.Id == id);
+        }
+    }
+}
